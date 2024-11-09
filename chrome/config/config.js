37108b4a20196303e29d76e5761199989f13041a
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-09 17:07:53 -0500

    add config files

---
+++ chrome/config/config.js
@@ -0,0 +1,58 @@
+const siteConfigs = {
+  // showroom === bottom of viewport
+  // showroom_panel === search modal
+  "songsterr.com": {
+    selectors: ["#showroom", "#showroom_panel"],
+  },
+
+  // tabs.ultimate-guitar.com:
+  // tYMPb, aV0z9 === popup video if adblocker is detected
+  // BIfOl === "view offical tab" popup
+  // XJpee === popup video
+  // CiMr5 === left sidebar
+
+  // ultimate-guitar.com:
+  // js-ab-regular === header
+  // kCj2L === footer
+  "ultimate-guitar.com": {
+    selectors: [
+      ".tYMPb",
+      ".aV0z9",
+      ".BIfOl",
+      ".XJpee",
+      ".CiMr5",
+      ".js-ab-regular",
+      ".kCj2L",
+    ],
+  },
+
+  // songsterrAd === bottom of page on tab pages
+  // adsbygoogle === all ads
+  "bigbasstabs.com": {
+    selectors: [".adsbygoogle", ".songsterrAd"],
+  },
+
+  // ads_banner === all ads
+  // popup-nps-btn === "report bad ads"
+  // js-ad-fake-player === "learn to play" on tab pages
+  "911tabs.com": {
+    selectors: [".ads_banner", ".popup-nps-btn", ".js-ad-fake-player"],
+  },
+
+  // d1xpq3pb === all ads on tab pages
+  // aniBox === video popup, bottom of viewport on tab pages
+  "chordify.net": {
+    selectors: [".d1xpq3pb", "#aniBox"],
+  },
+
+  // iframe === all ads
+  // b-ad-fake-player === "learn to play" on tab pages
+  "guitartabs.cc": {
+    selectors: ["iframe", ".b-ad-fake-player"],
+  },
+};
+
+const getSite = (hostname) => {
+  const sites = Object.keys(siteConfigs);
+  return sites.find(site => hostname.includes(site)) || null;
+};
